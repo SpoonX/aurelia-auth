ac6ac27c58a5f29596d5f8c2fa3a543d88293e2e
doktordirk <doktordirk@outlook.de>
2016-04-15 11:39:35 +0200

    feat(value-converters): make configurable which are added to globalResources

--- src/aurelia-authentication.js
+++ src/aurelia-authentication.js
@@ -1,6 +1,7 @@
 import {HttpClient} from 'aurelia-fetch-client';
 import {Config, Rest} from 'aurelia-api';
 import {PLATFORM} from 'aurelia-pal';
+import * as LogManager from 'aurelia-logging';
 
 import {AuthService} from './authService';
 import {AuthorizeStep} from './authorizeStep';
@@ -23,10 +24,6 @@ function configure(aurelia, config) {
     PLATFORM.location.origin = PLATFORM.location.protocol + '//' + PLATFORM.location.hostname + (PLATFORM.location.port ? ':' + PLATFORM.location.port : '');
   }
 
-  aurelia.globalResources('./value-converters/authFilter');
-  aurelia.globalResources('./value-converters/isRouteAuth');
-  aurelia.globalResources('./value-converters/isAuth');
-
   const baseConfig = aurelia.container.get(BaseConfig);
 
   if (typeof config === 'function') {
@@ -34,7 +31,12 @@ function configure(aurelia, config) {
   } else if (typeof config === 'object') {
     baseConfig.configure(config);
   }
+
   // after baseConfig was configured
+  for (let converter of baseConfig.globalValueConverters) {
+    aurelia.globalResources(`./value-converters/${converter}`);
+    LogManager.getLogger('authentication').info(`Add globalResources value-converter: ${converter}`);
+  }
   const fetchConfig  = aurelia.container.get(FetchConfig);
   const clientConfig = aurelia.container.get(Config);
 
